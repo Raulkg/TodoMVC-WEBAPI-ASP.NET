19ebf2cfab95abd54d047e7da097224c6e6afbd7
RahulkumarGaddam <rgaddam@syr.edu>
2016-10-25 17:46:43 -0400

    Database Created . Stable version of App skeleton. 10/25/2016 05:46PM

---
+++ TodoApp/TodoApp/TodoApp/TodoApp/App_Data/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [Key] VARCHAR(MAX) NULL, 
+    [Name] VARCHAR(MAX) NULL
+)
--- TodoApp/TodoApp/TodoApp/TodoApp/Models/Todoitem.cs
+++ TodoApp/TodoApp/TodoApp/TodoApp/Models/Todoitem.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace TodoApp.Models
 {
-    public class Todoitem
+    public class Todoitem 
     {
-
-        public string Key { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
+        public string Key {get;set ;}
+          public string Name {get;set; }
+          public bool IsComplete { get; set; }
+	}
 }
