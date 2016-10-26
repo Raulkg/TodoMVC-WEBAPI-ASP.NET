6bbe834f8b4d3a1867ffd7fff60ccb892d374342
RahulkumarGaddam <rgaddam@syr.edu>
2016-10-25 21:41:25 -0400

    Update Jquery Efforts Updated SQL Data Model Updated the Issue with ID management of Task

--- TodoApp/TodoApp/TodoApp/TodoApp/Controllers/WebapiController.cs
+++ TodoApp/TodoApp/TodoApp/TodoApp/Controllers/WebapiController.cs
@@ -23,10 +23,28 @@ namespace TodoApp.Controllers
               new Todoitem {Key ="4", Name = "eat", IsComplete = false} 
         };
 
-        public IEnumerable<Todoitem> GetAll()
+
+
+
+
+
+
+
+
+        public IEnumerable<MyDBModel> GetAll()
         {
-            return Todoitems;
+
+            List<MyDBModel> mytodoitems = new List<MyDBModel>();
+            using (todoDBEntities1 db = new todoDBEntities1())
+            {
+
+                mytodoitems = db.MyDBModels.OrderBy(a => a.Key).ToList();
+            }
+            return mytodoitems;
         }
+
+
+
         public IHttpActionResult GetByID(String id)
         {
             var todoitem = Todoitems.FirstOrDefault((p) => p.Key == id);
@@ -36,6 +54,116 @@ namespace TodoApp.Controllers
             }
             return Ok(todoitem);
         }
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody] MyDBModel item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            using (todoDBEntities1 db = new todoDBEntities1())
+            {
+
+               db.MyDBModels.Add(item);
+               try
+               {
+                   db.SaveChanges();
+               }
+
+               catch (Exception ec)
+               {
+                   Console.WriteLine(ec.Message);
+               }
+
+            }
+
+
+            return Ok(item);
+        }
+
+         [HttpPost]
+        public IHttpActionResult UpdateItem(int id ,MyDBModel details)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+             
+
+           
+             
+             using (todoDBEntities1 db = new todoDBEntities1())
+            {
+
+                db.Entry(details).State = System.Data.EntityState.Modified;
+             
+
+
+                try
+                {
+                    db.SaveChanges();
+                }
+
+                catch (Exception ec)
+                {
+                    Console.WriteLine(ec.Message);
+                }
+
+
+
+            }
+
+             return Ok(details);
+        }
+
+
+         [HttpDelete]
+         public IHttpActionResult DeleteItem(int id)
+         {
+             if (!ModelState.IsValid || id ==null)
+             {
+                 return BadRequest(ModelState);
+             }
+
+
+
+
+             using (todoDBEntities1 db = new todoDBEntities1())
+             {
+
+                 MyDBModel tmp = db.MyDBModels.Find(id);
+                 db.MyDBModels.Remove(tmp);
+
+
+
+                 try
+                 {
+                     db.SaveChanges();
+                 }
+
+                 catch (Exception ec)
+                 {
+                     Console.WriteLine(ec.Message);
+                 }
+
+
+
+             }
+
+             return Ok();
+         }
+
+
+
+
+
+
+
+
+
+
 
 
 
